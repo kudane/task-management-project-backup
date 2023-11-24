22da27a8768b8b28d8b2ce05ccb2c4985ed4cbe1
Kraisorn.R <kraisorn204@gmail.com>
2023-11-24 19:28:00 +0700

    [add] Winform.csproj

---
+++ backend/Winform/Program.cs
@@ -0,0 +1,19 @@
+﻿using Backend.Bussiness;
+using Backend.Persistence;
+
+using (var context = new TaskContext())
+{
+    context.Database.EnsureCreated();
+
+    var command = new ListTask.Command() { PriorityId = null, TypeId = null };
+    var service = new ListTask.Handler(context);
+    var response = service.Handle(command);
+
+    if (response != null)
+    {
+        foreach (var item in response.Items)
+        {
+            Console.WriteLine(item.Name);
+        }
+    }
+}
